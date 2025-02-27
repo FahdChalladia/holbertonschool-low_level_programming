ddce089bab150a39214f119d32286443115cf81c
fahdchalladia <fahdchalladia2004@gmail.com>
2025-02-27 10:29:25 +0100

    task1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to the integer to be updated
+ * @b: pointer to the integer to be updated
+ * Return: nothing
+ */
+void swap_int(int *a, int *b);
+{
+int c;
+c= *a ;
+*a = *b;
+*b = c;
+
+}
+
+
+
+
+
+
