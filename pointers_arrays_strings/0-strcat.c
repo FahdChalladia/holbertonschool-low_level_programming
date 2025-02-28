c96198422dbdc5ccbf2007f323dc949115c528ed
fahdchalladia <fahdchalladia2004@gmail.com>
2025-02-28 13:53:43 +0100

    idk

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,12 @@
 char *_strcat(char *dest, char *src)
 {
 int i = 0;
-while (dest[i] != "/0")
+while (dest[i] != "\0")
 {
 i++;
 }
 int j =  0;
-while (src[j] != "/0")
+while (src[j] != "\0")
 {
 *dest[i] = src[j];
 i++;
