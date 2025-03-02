debc7eb282a5abb67d876c0b913406342e5b11cf
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-02 14:01:10 +0100

    task5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,9 +9,11 @@
 char *string_toupper(char *str)
 {
 int i = 0;
-while (s[i] != '\0')
+while (str[i] != '\0')
+{
 if (str[i] >= 'a' && str[i] <= 'z')
-*str[i] -= 32;
+str[i] -= 32;
 i++;
-return (*str);
+}
+return (str);
 }
