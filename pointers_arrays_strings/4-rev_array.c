a7c2227c54c1e57bf6b06eb47c5f4cc225132bae
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-02 13:29:09 +0100

    task4 v2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -5,16 +5,18 @@
  * @a: the array of integers.
  * @n: the number of elements in the array.
  * Return: nothing.
- */
+*/
 void reverse_array(int *a, int n)
 {
-int *start = a;
-int *end = a + n - 1;
-int temp;
-while (start < end)
-temp = *start;
-*start = *end;
-*end = temp;
-start++;
-end--;
+int c[n];
+int j = 0;
+for (int i = n - 1; i >= 0; i--)
+{
+c[j] = a[i];
+j++;
+}
+for (int i = 0; i < n; i++)
+{
+a[i] = c[i];
+}
 }
