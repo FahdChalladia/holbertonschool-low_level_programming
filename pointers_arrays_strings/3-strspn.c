042dd9e145745f26bb77ca7e509cd231685d1ab4
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-04 12:26:49 +0100

    task3 v3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,6 +7,27 @@
  * @accept: pointer to characters to match
  * Return: Number of bytes in s consisting of only accept chars
  */
+
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int j = 0;
+while (s[j] != '\0')
+{
+if (_strchr(accept, s[j]) == NULL)
+return (j);
+j++;
+}
+return (j);
+}
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: pointer to the string
+ * @c: character to find
+ * Return: pointer to first occurrence of c, or NULL if not found
+ */
 char *_strchr(char *s, char c)
 {
 int i = 0;
@@ -19,16 +40,4 @@ i++;
 if (c == '\0')
 return (&s[i]);
 return (NULL);
-}
-
-unsigned int _strspn(char *s, char *accept)
-{
-unsigned int j = 0;
-while (s[j] != '\0')
-{
-if (_strchr(accept, s[j]) == NULL)
-return (j);
-j++;
-}
-return (j);
 }
