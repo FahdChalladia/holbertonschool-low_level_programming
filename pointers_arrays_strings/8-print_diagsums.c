4659169d2aa1087d5e8f797c922cee23c221f952
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-06 10:37:46 +0100

    task8 v2

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,8 +14,8 @@ for (i = 0; i < size; i++)
 sum1 += a[i * size + i];
 sum2 += a[i * size + (size - 1 - i)];
 }
-_putchar(sum1);
-_putchar(" , ");
-_putchar(sum2);
+_putchar(sum1+'0');
+_putchar(",");
+_putchar(sum2+'0');
 _putchar('\n');
 }
