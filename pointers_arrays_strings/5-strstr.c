72df376a967edbeca10791499da4e005588bce6f
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-06 10:06:11 +0100

    task5 v3

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+
+/**
+ * _strstr - Locates a string in a string
+ * @haystack: pointer to the string
+ * @needle: pointer  to the key string
+ * Return: pointer to first occurrence of c, or NULL if not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+if (!*needle)
+return (haystack);
+while (*haystack)
+{
+char *h = haystack;
+char *n = needle;
+while (*h && *n && (*h == *n))
+{
+h++;
+n++;
+}
+if (!*n)
+return (haystack);
+haystack++;
+}
+return (NULL);
+}
