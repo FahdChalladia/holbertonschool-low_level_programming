5521bd7c00976bd890c526a76183aa36bcdaf097
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-07 17:12:54 +0100

    new update v3

---
+++ 0x04-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - Converts all lowercase letters of a string to uppercase.
+ * @str: Pointer to the string to be modified.
+ *
+ * Return: The modified string.
+ */
+char *string_toupper(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] >= 'a' && str[i] <= 'z')
+str[i] -= 32;
+i++;
+}
+return (str);
+}
---
+++ 0x04-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337 (leet speak).
+ * @str: The string to be encoded.
+ *
+ * Return: The encoded string.
+ */
+char *leet(char *str)
+{
+char replacements[] = {'4', '3', '0', '7', '1'};
+char letters[] = {'a', 'e', 'o', 't', 'l'};
+int i, j;
+for (i = 0; str[i] != '\0'; i++)
+{
+for (j = 0; j < 5; j++)
+{
+if (str[i] == letters[j] || str[i] == letters[j] - 32)
+{
+str[i] = replacements[j];
+break;
+}
+}
+}
+return (str);
+}
