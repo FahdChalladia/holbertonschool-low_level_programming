ed958cbc6707197df9686a51b3a94a4ade060a64
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-07 17:10:49 +0100

    new update v3

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - asd
+ * Return: sad
+ */
+int main(void)
+{
+int i;
+int x = 0;
+for (i = 1; i <= 100; i++)
+{
+if (((i % 10) + (i / 10)) % 3 == 0)
+{
+printf("Fizz");
+x = 1;
+}
+if ((i % 10 == 5) || (i % 10 == 0))
+{
+printf("Buzz");
+x = 1;
+}
+if (x == 0)
+printf("%d", i);
+x = 0;
+if (i != 100)
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
