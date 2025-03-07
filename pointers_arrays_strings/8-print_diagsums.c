6f95a4db29587ef9701eafeb57040564018b6e00
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-07 13:33:32 +0100

    task8 v3

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -15,7 +15,7 @@ if (n / 10)
 print_number(n / 10);
 _putchar((n % 10) + '0');
 }
- 
+
 /**
  * print_diagsums - Prints the sum of the two diagonals of a square matrix
  * @a: Pointer to the first element of the matrix
@@ -24,13 +24,13 @@ _putchar((n % 10) + '0');
 void print_diagsums(int *a, int size)
 {
 int i, sum1 = 0, sum2 = 0;
- for (i = 0; i < size; i++)
+for (i = 0; i < size; i++)
 {
-sum1 += a[i * size + i]; // Primary diagonal
-sum2 += a[i * size + (size - 1 - i)]; // Secondary diagonal
+sum1 += a[i * size + i];
+sum2 += a[i * size + (size - 1 - i)];
 }
- 
-print_number(sum1); 
+
+print_number(sum1);
 _putchar(',');
 _putchar(' ');
 print_number(sum2);
