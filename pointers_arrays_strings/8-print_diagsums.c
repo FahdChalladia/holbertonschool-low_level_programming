ac38dfade54b53e89c0214a7d49ded0d22bb1bcd
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-07 13:31:27 +0100

    task8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,6 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * print_number - Helper function to print an integer using _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10)
+print_number(n / 10);
+_putchar((n % 10) + '0');
+}
+ 
 /**
  * print_diagsums - Prints the sum of the two diagonals of a square matrix
  * @a: Pointer to the first element of the matrix
@@ -9,14 +24,15 @@
 void print_diagsums(int *a, int size)
 {
 int i, sum1 = 0, sum2 = 0;
-for (i = 0; i < size; i++)
+ for (i = 0; i < size; i++)
 {
-sum1 += a[i * size + i];
-sum2 += a[i * size + (size - 1 - i)];
+sum1 += a[i * size + i]; // Primary diagonal
+sum2 += a[i * size + (size - 1 - i)]; // Secondary diagonal
 }
-_putchar(sum1 + '0');
+ 
+print_number(sum1); 
 _putchar(',');
 _putchar(' ');
-_putchar(sum2 + '0');
+print_number(sum2);
 _putchar('\n');
 }
