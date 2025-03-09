e51e1d997cc4b6d48a4e17c08dcfba862dcb5bd6
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-09 15:02:06 +0100

    task1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion
+ * @s: The string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+
+_puts_recursion(s + 1);
+_putchar(*s);
+}
