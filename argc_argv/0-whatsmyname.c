bc77258c266e6e96e0c9e15dc4b2a7b6583fc656
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-09 22:51:09 +0100

    task0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+char *program_name = argv[0];
+while (*program_name)
+{
+_putchar(*program_name);
+program_name++;
+}
+_putchar('\n');
+return (0);
+}
