cff061ec2eadde5522ba9e0763bdf477a971e2e2
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-10 12:33:54 +0100

    task1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * print_number - prints an integer to stdout using _putchar
+ * @n: The integer to print
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n == 0)
+{
+_putchar('0');
+return;
+}
+if (n / 10 != 0)
+{
+print_number(n / 10);
+}
+_putchar((n % 10) + '0');
+}
+
+
+/**
  * main - Entry point of the program
  *
  * This function prints the program name followed by a newline.
