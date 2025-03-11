a475df624630615e983b7c49f616c607ff1b966a
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-11 10:37:53 +0100

    v5 : betty leave me alone please :'(

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-int num1 , num2 ;
+int num1, num2;
 if (argc != 3)
 {
 printf("Error\n");
@@ -19,6 +19,6 @@ return (1);
 }
 num1 = atoi(argv[1]);
 num2 = atoi(argv[2]);
-printf("%d\n", num1 * num2);
+printf("%d\n", num1 *num2);
 return (0);
 }
