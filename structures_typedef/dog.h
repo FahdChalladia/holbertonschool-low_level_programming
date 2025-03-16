5fb33eae26aa59926ab61ca439c43ad2183c0e3a
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-16 22:58:25 +0100

    task0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * struct dog - Defines a dog's basic information.
+ * @name: The dog's name.
+ * @age: The dog's age.
+ * @owner: The dog's owner.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
