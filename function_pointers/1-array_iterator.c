f09f37342a9047150249dd8d984a082c48dcd8cf
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-22 12:02:51 +0100

    define conflict

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-#include <stddef.h>
+
 
 /**
  * array_iterator - Executes a function given as a parameter on an array.
@@ -9,9 +9,10 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+size_t i;
 if (array == NULL || action == NULL)
 return;
-for (size_t i = 0; i < size; i++)
+for ( i = 0; i < size; i++)
 {
 action(array[i]);
 }
