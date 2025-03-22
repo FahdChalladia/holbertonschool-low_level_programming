92dde557a3e7a6d3be0789c05c33cdc987bcb491
fahdchalladia <fahdchalladia2004@gmail.com>
2025-03-22 13:30:44 +0100

    task3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,18 +1,18 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
 /**
-*print_all - Prints anything, based on a format string.
-*@format: A string containing the types of arguments passed to the function.
-*          'c' for char, 'i' for int, 'f' for float, 's' for string.
-*          If a string is NULL, it prints "(nil)".
+* print_all - Prints anything, based on a format string.
+* @format: A string containing the types of arguments passed to the function.
+*           'c' for char, 'i' for int, 'f' for float, 's' for string.
+*           If a string is NULL, it prints "(nil)".
 */
 void print_all(const char * const format, ...)
 {
 unsigned int i = 0;
 char *str;
 va_list args;
+int first = 1;
 
 if (format == NULL)
 {
@@ -24,28 +24,30 @@ va_start(args, format);
 
 while (format[i])
 {
-if (format[i] == 'c')
-printf("%c", va_arg(args, int)); /* char is promoted to int */
-else if (format[i] == 'i')
-printf("%d", va_arg(args, int));
-else if (format[i] == 'f')
-printf("%f", va_arg(args, double));
-else if (format[i] == 's')
+if (!first)
+printf(", ");
+switch (format[i])
 {
+case 'c':
+printf("%c", va_arg(args, int)); /* char is promoted to int */
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char*);
 if (str == NULL)
 str = "(nil)";
 printf("%s", str);
+break;
 }
-
-if (format[i + 1] && (format[i] == 'c' || format[i] == 'i' || 
-format[i] == 'f' || format[i] == 's'))
-printf(", ");
-
+first = 0;
 i++;
 }
 
 va_end(args);
-
 printf("\n");
 }
