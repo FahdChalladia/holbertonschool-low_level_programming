f398912245268f4eb26a0eb9d9048f51ba232bec
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-14 14:05:29 +0100

    task2 and 3 name modifications

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 /**
- * create_file - Creates a file and writes text to it.
- * @filename: Name of the file to create.
- * @text_content: NULL-terminated string to write to the file.
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file.
+ * @text_content: NULL-terminated string to append.
  *
  * Return: 1 on success, -1 on failure.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int fd;
 ssize_t w;
@@ -18,7 +18,7 @@ size_t len = 0;
 if (filename == NULL)
 return (-1);
 
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 
