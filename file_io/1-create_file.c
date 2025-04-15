ef5d075760dc4ef312d5b9641801ed38410de09d
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-15 10:36:21 +0100

    clear file if it does exist

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ if (filename == NULL)
 {
 return (-1);
 }
-fd = open(filename, O_WRONLY | O_CREAT, 0600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 else if (text_content == NULL)
