e481444a6d848e72632e07b30b5771d44171a7de
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-16 08:43:39 +0100

    get old version back

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,20 +17,20 @@ int fd_from, fd_to, r, w;
 char buf[BUF_SIZE];
 if (argc != 3)
 {
-dprintf(STDOUT_FILENO, "Usage: cp %s %s\n", "file_from", "file_to");
+dprintf(2, "Usage: cp %s %s\n", "file_from", "file_to");
 exit(97);
 }
 fd_from = open(argv[1], O_RDONLY);
 if (fd_from == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (fd_to == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
 close(fd_from);
 exit(99);
 }
@@ -39,25 +39,25 @@ while ((r = read(fd_from, buf, BUF_SIZE)) > 0)
 w = write(fd_to, buf, r);
 if (w != r)
 {
-dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
+dprintf(2, "Error: Can't write to %s\n", argv[2]);
 close(fd_from), close(fd_to);
 exit(99);
 }
 }
 if (r == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 close(fd_from), close(fd_to);
 exit(98);
 }
 if (close(fd_from) == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd_from);
+dprintf(2, "Error: Can't close fd %d\n", fd_from);
 exit(100);
 }
 if (close(fd_to) == -1)
 {
-dprintf(STDOUT_FILENO, "Error: Can't close fd %d\n", fd_to);
+dprintf(2, "Error: Can't close fd %d\n", fd_to);
 exit(100);
 }
 return (0);
