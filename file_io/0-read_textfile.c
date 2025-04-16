51868e5611e9f1782f48229dc0fe833a89f0c5a5
fahdchalladia <fahdchalladia2004@gmail.com>
2025-04-16 09:30:15 +0100

    final version

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+
 /**
  * read_textfile - Reads a text file and prints it to the POSIX
  * @filename: pointer to the filename
@@ -10,31 +11,39 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char buffer[1024];
+char *buffer;
 ssize_t bytesRead, bytesWritten;
 int fd;
 if (filename == NULL)
 {
 return (0);
 }
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+return (0);
+}
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 {
+free(buffer);
 return (0);
 }
 bytesRead = read(fd, buffer, letters);
 if (bytesRead == -1)
 {
+free(buffer);
 close(fd);
 return (0);
 }
-
 bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 if (bytesWritten != bytesRead)
 {
-    close(fd);
-    return (0);
+free(buffer);
+close(fd);
+return (0);
 }
 close(fd);
+free(buffer);
 return (bytesRead);
 }
